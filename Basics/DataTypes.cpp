41ad67176998696cb35b7fe73b830a21e7c21469
Lijo Joseph <lijojoseph234@gmail.com>
2024-01-23 19:55:54 +0530

    Basics day1

---
+++ Basics/DataTypes.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // integer data type
+    int a = 5;
+    cout << a << endl;
+
+
+    // Character data type
+    char ch = 'd';
+    cout<< ch << endl;
+
+    // boolean 
+    bool bl = false;
+    cout<< bl << endl;
+
+    // float
+    float f = 1.2;
+    cout<< f << endl; 
+
+    //double
+    double d = 1.23455;
+    cout<< d << endl;
+
+    // to check the size of the variable
+    int size = sizeof(d);
+    cout << size << endl;
+
+    
+    
+    
+}
