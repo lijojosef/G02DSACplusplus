a4da553ad38f0555ad74ec50d29c17f59719e4ec
Lijo Joseph <lijojoseph234@gmail.com>
2024-01-25 20:58:00 +0530

    pattern day4

---
+++ Pattern/Pattern2.cpp
@@ -0,0 +1,27 @@
+/*
+    111
+    222
+    333
+
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    int i = 1;
+    while(i <= n){
+        int j = 1;
+        while( j <= n){
+            cout<<i;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    return 0;
+}
