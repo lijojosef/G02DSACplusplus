03b717aff22e294f59ff619407847094b8b2258b
Lijo Joseph <lijojoseph234@gmail.com>
2024-01-25 18:49:00 +0530

    Basics day2

---
+++ Basics/PositiveNegative.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin>>n;
+
+    if(n > 0)
+    {
+        cout<<"Answer is positive" << endl;
+    }
+    if(n < 0){
+        cout << "Answer is negative" << endl;
+    }
+    
+    return 0;
+}
---
+++ Basics/PositiveNegativeMult.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a, b;
+    // cin >> a >> b;
+
+    // if( a > b)
+    // {
+    //     cout<< "value of a is: " << a <<endl;
+    // }else{
+    //     cout<< "value of b is: " <<  b << endl;
+    // }
+    
+    int a = 9;
+    
+    if(a > 0){
+        cout<<"A is positive " <<endl;
+    }else if (a < 0){
+        cout<< " A is negative "<<endl;
+    }else{
+        cout<< "A is zero" <<endl;
+    }
+
+    
+    return 0;
+
+
+}
